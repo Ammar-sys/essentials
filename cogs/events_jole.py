28af68824e25723aed59ec1c7cb4bb2f084b22c5
Ammar-sys <69536330+Ammar-sys@users.noreply.github.com>
2021-02-06 23:11:04 +0100

    Update events_jole.py

--- cogs/events_jole.py
+++ cogs/events_jole.py
@@ -1,7 +1,7 @@
 from discord.ext import commands
 
 def sqlify(path):
-    return ''.join(open(path, 'r').readlines())
+    return open(path, 'r').read()
 
 class Event_jole(commands.Cog):
     def __init__(self, bot):
@@ -9,11 +9,23 @@ class Event_jole(commands.Cog):
 
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
-        await self.bot.pool.execute(sqlify('./sql_files/guild_join.sql'), False, guild.id, None, None, None)
+        async with self.bot.pool.acquire() as connection:
+            async with connection.transaction():
+                await connection.execute(sqlify('./sql_files/_main/_guild_join.sql'), guild.id, '.')
+
+                await connection.execute(sqlify('./sql_files/modules/dis_ver_join.sql'), None, None, None, guild.id)
+                await connection.execute(sqlify('./sql_files/modules/roblox_ver_join.sql'), None, None, None, guild.id)
+                await connection.execute(sqlify('./sql_files/modules/ticket_sys_join.sql'), False, guild.id, None, None, None, None)
 
     @commands.Cog.listener()
     async def on_guild_remove(self, guild):
-        await self.bot.pool.execute(sqlify('./sql_files/guild_leave.sql'), guild.id)
+        async with self.bot.pool.acquire() as connection:
+            async with connection.transaction():
+                await connection.execute(sqlify('./sql_files/_main/_guild_leave.sql'), guild.id)
+
+                await connection.execute(sqlify('./sql_files/modules/dis_ver_leave.sql'), guild.id)
+                await connection.execute(sqlify('./sql_files/modules/roblox_ver_leave.sql'), guild.id)
+                await connection.execute(sqlify('./sql_files/modules/ticket_sys_leave.sql'), guild.id)
 
 def setup(bot):
     bot.add_cog(Event_jole(bot))
